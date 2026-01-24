fc69b8dca3fd98c151effceebf8e13afd0a3e4d0
Alve-Camia <gaalve@evc.pshs.edu.ph>
2026-01-24 08:21:15 +0800

    Added Project Code v0.1.1
    The third version of the program uploaded is focused on improving the readability and maintainability of the program by utilizing frames and a function that changes which frame is visible. Additionally, the update makes the clock in the main menu now auto-update.

--- Project's Code/Program Code.py
+++ Project's Code/Program Code.py
@@ -5,55 +5,23 @@ from datetime import datetime # For checking log in time
 
 def demo_msg():
     demo_label.config(text="Still in development...")
-    menu_btn.pack(pady=5)
 
 def main_menu():
-    msg_label.config(text="")
-    demo_label.config(text="")
-    menu_btn.pack_forget()
-    attendance_label.pack_forget()
-    attendance_create_btn.pack_forget()
-    attendance_view_btn.pack_forget()
-    attendance_fillout_btn.pack_forget()
-
-    menu_time_label.pack(pady=5)
-
-    main_menu_label.pack(pady=10)
-    attendance_btn.pack(pady=5)
-    
-    
-    logout_btn.pack(pady=5)
+    show_frame(menu_frame)
 
 def attendance_menu():
-    menu_time_label.pack_forget()
-    main_menu_label.pack_forget()
-    logout_btn.pack_forget()
-    attendance_btn.pack_forget()
-
-    
-    attendance_label.pack(pady=5)
-    attendance_create_btn.pack(pady=5)
-    attendance_view_btn.pack(pady=5)
-    attendance_fillout_btn.pack(pady=5)
-
+    show_frame(attendance_frame)
 
 def logout():
-    
     msg_label.config(text="")
     username_box.delete(0, tk.END)
     password_box.delete(0, tk.END)
-    menu_time_label.pack_forget()
-    
-    username_label.pack(pady=(10,3))
-    username_box.pack(pady=(3,10))
-    password_label.pack(pady=(5,3))
-    password_box.pack(pady=(3,10))
-    signup_btn.pack(pady=5)
-    login_btn.pack(pady=5)
-    
-    logout_btn.pack_forget()
-    main_menu_label.pack_forget()
-    attendance_btn.pack_forget()
+    show_frame(login_frame)
+
+def show_frame(frame): # This takes in the argument when the function was called
+    for f in (login_frame, menu_frame, attendance_frame): # List of frames that f is part of
+        f.pack_forget() # Makes all frames not visible
+    frame.pack(pady=20) # Packs the frame that was used an argument in the function
 
 def signup():
     username = username_box.get()
@@ -105,12 +73,7 @@ def login():
     if found:
         time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         msg_label.config(text=username + " logged IN at " + time)
-        username_label.pack_forget()
-        username_box.pack_forget()
-        password_label.pack_forget()
-        password_box.pack_forget()
-        signup_btn.pack_forget()
-        login_btn.pack_forget()
+        show_frame(menu_frame)
 
         menu_btn.pack(pady=5)
         
@@ -119,51 +82,74 @@ def login():
     else:
         messagebox.showwarning("Error", "Invalid account name or password. Please try again.")
 
-#GUI
-
+# Framework of the program
 win = tk.Tk()
 win.title("Attendance Checker")
 win.geometry("500x500")
 
+# Instantiates the frames
+login_frame = tk.Frame(win)
+menu_frame = tk.Frame(win)
+attendance_frame = tk.Frame(win)
+
 title = tk.Label(win, text="Attendance Checker", font=("Montserrat", 24))
 title.pack(pady=(20,10))
 
-# Widgets: Username
-username_label = tk.Label(win, text="Enter Account Name (letters only):", font=("Arial", 12))
+menu_time = datetime.now().strftime("%B %d, %Y - %H:%M:%S (%a)")
+
+#Configs the menu_time label after 1 sec
+def update_clock():
+    current_time = datetime.now().strftime("%B %d, %Y - %H:%M:%S (%a)")
+    menu_time_label.config(text=f"Current time: {current_time}")
+    win.after(1000, update_clock) #Configs the menu_time label after 1 sec
+
+#Login Frame
+username_label = tk.Label(login_frame, text="Enter Account Name (letters only):", font=("Arial", 12))
+username_box = tk.Entry(login_frame, width=30)
+
+password_label = tk.Label(login_frame, text="Enter Password (must contain 8 characters or more):", font=("Arial", 12))
+password_box = tk.Entry(login_frame, width=30, show="*")
+
+signup_btn = tk.Button(login_frame, text="Sign Up", width=15, font=("Montserrat", 12), command=signup)
+login_btn = tk.Button(login_frame, text="Log In", width=15, font=("Montserrat", 12), command=login)
+
 username_label.pack(pady=(10,3))
-username_box = tk.Entry(win, width=30)
 username_box.pack(pady=(3,10))
-
-# Widgets: Password
-password_label = tk.Label(win, text="Enter Password (must contain 8 characters or more):", font=("Arial", 12))
 password_label.pack(pady=(5,3))
-password_box = tk.Entry(win, width=30, show="*")
 password_box.pack(pady=(3,10))
-
-# Widgets: Sign up and Log in 
-signup_btn = tk.Button(win, text="Sign Up", width=15, font=("Montserrat", 12), command=signup)
 signup_btn.pack(pady=5)
-
-login_btn = tk.Button(win, text="Log In", width=15, font=("Montserrat", 12), command=login)
 login_btn.pack(pady=5)
 
-menu_btn = tk.Button(win, text="Main menu", width=15, font=("Montserrat", 12), command=main_menu)
-main_menu_label = tk.Label(win, text="Main Menu", font=("Arial", 12))
-attendance_btn = tk.Button(win, text="Attendance", width=15, font=("Montserrat", 12), command=attendance_menu)
-logout_btn = tk.Button(win, text="Log Out", width=15, font=("Montserrat", 12), command=logout)
-menu_time = datetime.now().strftime("%B %d, %Y - %H:%M:%S (%a)")
-menu_time_label = tk.Label(win, text=f"Current time: {menu_time}", font=("Arial", 12))
+#Main Menu Frame
+msg_label = tk.Label(menu_frame, text="", fg="green", font=("Arial", 12))
+menu_time_label = tk.Label(menu_frame, text=f"Current time: {menu_time}", font=("Arial", 12))
+main_menu_label = tk.Label(menu_frame, text="Main Menu", font=("Arial", 12))
+attendance_btn = tk.Button(menu_frame, text="Attendance", width=15, font=("Montserrat", 12), command=attendance_menu)
+logout_btn = tk.Button(menu_frame, text="Log Out", width=15, font=("Montserrat", 12), command=logout)
 
-attendance_label = tk.Label(win, text="Attendance Options", font=("Arial", 12))
-attendance_create_btn = tk.Button(win, text="Create Attendance", width=15, font=("Montserrat", 12), command=demo_msg)
-attendance_view_btn = tk.Button(win, text="View Attendances", width=15, font=("Montserrat", 12), command=demo_msg)
-attendance_fillout_btn = tk.Button(win, text="Fill out Attendance", width=15, font=("Montserrat", 12), command=demo_msg)
+msg_label.pack(pady=5)
+menu_time_label.pack(pady=5)
+main_menu_label.pack(pady=5)
+attendance_btn.pack(pady=5)
+logout_btn.pack(pady=5)
 
-#msg
-msg_label = tk.Label(win, text="", fg="green", font=("Arial", 12))
-msg_label.pack(pady=10)
 
-demo_label = tk.Label(win, text="", fg="orange", font=("Arial", 12))
-demo_label.pack(pady=10)
+#Attendance Frame
+demo_label = tk.Label(attendance_frame, text="", fg="orange", font=("Arial", 12))
+attendance_label = tk.Label(attendance_frame, text="Attendance Options", font=("Arial", 12))
+attendance_create_btn = tk.Button(attendance_frame, text="Create Attendance", width=15, font=("Montserrat", 12), command=demo_msg)
+attendance_view_btn = tk.Button(attendance_frame, text="View Attendances", width=15, font=("Montserrat", 12), command=demo_msg)
+attendance_fillout_btn = tk.Button(attendance_frame, text="Fill out Attendance", width=15, font=("Montserrat", 12), command=demo_msg)
+menu_btn = tk.Button(attendance_frame, text="Main menu", width=15, font=("Montserrat", 12), command=main_menu)
 
+demo_label.pack(pady=5)
+attendance_label.pack(pady=5)
+attendance_create_btn.pack(pady=5)
+attendance_view_btn.pack(pady=5)
+attendance_fillout_btn.pack(pady=5)
+menu_btn.pack(pady=5)
+
+show_frame(login_frame) 
+update_clock()
 win.mainloop()
+
